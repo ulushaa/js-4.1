5ebbcd6b2cbe2b3d58c6e329817526c766351161
ulusha <ummelkhatov@gmail.com>
2021-12-31 01:23:44 +0500

    test

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function jojo(){
+    let my = prompt('введите своё имя');
+    let year = +prompt('введите год вашего рождение');
+    let now = +prompt('введите текуший год');
+    let result = now - year;
+    return (my+ ', ваш возраст '+result+' И С НОВЫМ ГОДОМ');
+}
+alert(jojo())
+
+
+
+
+
+
+
+ 
